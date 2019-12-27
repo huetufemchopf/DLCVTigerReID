0f8a05e521de898a718bc4586af7e871e7207862
huetufemchopf <celine.nauer@gmail.com>
2019-12-27 17:43:43 +0800

    added feature extractor

---
+++ featureextractor.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import datasets, models, transforms
+
+class FeatureExtractor(nn.Module):
+
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+
+        self.resnet50 = models.resnet50(pretrained=True)
+        vec_length = self.resnet50.fc.in_features
+        self.resnet50.fc = nn.Linear(vec_length, 2048)
+
+    def forward(self, im):
+        x = self.resnet50(im)
+        return x
+
+
