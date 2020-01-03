398e41ea6c9954aa4a795b95964c9ab2491010fb
Celine Nauer <cenauer@cml24.csie.ntu.edu.tw>
2020-01-03 14:41:00 +0800

    changed settings for 2 gpus

--- featureextractor.py
+++ featureextractor.py
@@ -1,7 +1,7 @@
-import torch
-import torch.nn as nn
-from torchvision import models
-import torch.nn.init as init
+import torch 
+import torch.nn as nn 
+from torchvision import models 
+import torch.nn.init as init 
 import torch.nn.functional as F
 
 
