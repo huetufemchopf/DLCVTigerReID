c6de9821f3d9a0f42c421336af52b6788c76025b
cmaro2 <c.marzalr@alumnos.upm.es>
2020-01-13 17:07:56 +0800

    Final commit only missing model

--- featureextractor.py
+++ featureextractor.py
@@ -9,12 +9,12 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self, local_conv_out_channels=128, num_classes=1000):
         super(Model, self).__init__()
-        self.base = models.resnet50(pretrained=True)
-        self.base = nn.Sequential(*list(self.base.children())[:-2])
-        planes = 2048
+        #self.base = models.resnet50(pretrained=True)
+        #self.base = nn.Sequential(*list(self.base.children())[:-2])
+        #planes = 2048
 
-        #self.base = models.densenet121(pretrained=True).features
-        #planes = 1024
+        self.base = models.densenet121(pretrained=True).features
+        planes = 1024
 
         self.bn = nn.BatchNorm1d(planes)
 
